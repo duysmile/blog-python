63c0548c35cd0ddd8e55ea971bbd68b0ac190c7f
duy.nguyen <duy.nguyen@vnlp.ai>
2021-06-18 16:00:02 +0700

    add register google api

--- app/modules/auth/biz/register_biz.py
+++ app/modules/auth/biz/register_biz.py
@@ -7,11 +7,20 @@ URL = "https://0ce0e595159c.ngrok.io"
 
 FB_CLIENT_ID = os.environ.get("FB_CLIENT_ID")
 FB_CLIENT_SECRET = os.environ.get("FB_CLIENT_SECRET")
+GG_CLIENT_ID = os.environ.get("GG_CLIENT_ID")
+GG_CLIENT_SECRET = os.environ.get("GG_CLIENT_SECRET")
 
 FB_AUTHORIZATION_BASE_URL = "https://www.facebook.com/dialog/oauth"
 FB_TOKEN_URL = "https://graph.facebook.com/oauth/access_token"
+GG_AUTHORIZATION_BASE_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+GG_TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token"
 
 FB_SCOPE = ["email"]
+GG_SCOPE = [
+    "openid",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+]
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
@@ -19,15 +28,33 @@ from ..model.user import User
 
 def get_auth_url_fb():
     facebook = requests_oauthlib.OAuth2Session(
-        FB_CLIENT_ID, redirect_uri=URL + "/v1/fb-callback", scope=FB_SCOPE
+        FB_CLIENT_ID,
+        redirect_uri=URL + "/v1/facebook/callback",
+        scope=FB_SCOPE,
     )
     authorization_url, _ = facebook.authorization_url(FB_AUTHORIZATION_BASE_URL)
 
     return authorization_url
 
+def get_auth_url_gg():
+    google = requests_oauthlib.OAuth2Session(
+        GG_CLIENT_ID,
+        redirect_uri=URL + "/v1/google/callback",
+        scope=GG_SCOPE,
+    )
+    authorization_url, _ = google.authorization_url(
+        GG_AUTHORIZATION_BASE_URL,
+        access_type="offline",
+        prompt="select_account"
+    )
+
+    return authorization_url
+
 def handle_fb_callback(path):
     facebook = requests_oauthlib.OAuth2Session(
-        FB_CLIENT_ID, scope=FB_SCOPE, redirect_uri=URL + "/v1/fb-callback"
+        FB_CLIENT_ID,
+        scope=FB_SCOPE,
+        redirect_uri=URL + "/v1/facebook/callback",
     )
 
     # we need to apply a fix for Facebook here
@@ -42,12 +69,36 @@ def handle_fb_callback(path):
 
     # Fetch a user resource
     facebook_user_data = facebook.get(
-        "https://graph.facebook.com/me?fields=id,name,email,picture{url}"
+        "https://graph.facebook.com/me?fields=id,email"
     ).json()
 
     email = facebook_user_data.get("email")
 
     return email
+
+def handle_gg_callback(path):
+    google = requests_oauthlib.OAuth2Session(
+        GG_CLIENT_ID,
+        redirect_uri=URL + "/v1/google/callback",
+        scope=GG_SCOPE,
+    )
+
+    # get access token
+    google.fetch_token(
+        GG_TOKEN_URL,
+        client_secret=GG_CLIENT_SECRET,
+        authorization_response=URL + path,
+    )
+
+    # Fetch a user resource
+    google_user_data = google.get(
+        "https://www.googleapis.com/oauth2/v1/userinfo"
+    ).json()
+
+    email = google_user_data.get("email")
+
+    return email
+
 
 def create_user(email, provider):
     exist_user = User.query.filter_by(email=email).first()
@@ -60,5 +111,3 @@ def create_user(email, provider):
     ).create()
 
     return None
-
-
--- app/modules/auth/transport/register.py
+++ app/modules/auth/transport/register.py
@@ -3,7 +3,7 @@ import flask
 import logging
 
 from app.modules.auth.model.user import User
-from ..biz.register_biz import get_auth_url_fb, handle_fb_callback, create_user
+from ..biz.register_biz import get_auth_url_fb, handle_fb_callback, create_user, get_auth_url_gg, handle_gg_callback
 
 auth_api = Blueprint("authentication", __name__)
 
@@ -19,9 +19,9 @@ def register():
         if provider == "facebook":
             authorize_url = get_auth_url_fb()
         elif provider == "google":
-            pass
+            authorize_url = get_auth_url_gg()
         else:
-            return make_response(jsonify({"data": "must provide provider"}), 400)
+            return make_response(jsonify({"error": "must provide provider"}), 400)
 
         return make_response(jsonify({"data": authorize_url}), 200)
     except Exception as e:
@@ -29,11 +29,18 @@ def register():
         return make_response(jsonify({"error": "cannot create auth url for facebook"}), 500)
 
 
-@auth_api.route("/fb-callback", methods=["GET", "POST"])
-def fb_callback():
+@auth_api.route("/<provider>/callback", methods=["GET", "POST"])
+def fb_callback(provider):
     try:
-        email = handle_fb_callback(flask.request.full_path)
-        error = create_user(email, "facebook")
+        email = ""
+        if provider == "facebook":
+            email = handle_fb_callback(flask.request.full_path)
+        elif provider == "google":
+            email = handle_gg_callback(flask.request.full_path)
+        else:
+            return make_response(jsonify({"error": "invalid provider"}), 400)
+
+        error = create_user(email, provider)
         if error is not None:
             return make_response(jsonify({"error": str(error)}), 400)
         return make_response(jsonify({"data": True}), 200)
