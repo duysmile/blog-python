5a70dda3d99b288fcc8a5dc29b950ef4cfaefce1
duy.nguyen <duy.nguyen@vnlp.ai>
2021-06-19 21:56:55 +0700

    add api get detail post with like

--- app/route.py
+++ app/route.py
@@ -4,6 +4,7 @@ from app.modules.post.transport.create_post import create_post_api
 from app.modules.post.transport.list_post import list_post_api
 from app.modules.post.transport.list_post_by_user import list_post_by_user_api
 from app.modules.like.transport.like_post import like_post_api
+from app.modules.post.transport.get_post import get_post_api
 
 def init_route(app):
     app.register_blueprint(auth_api, url_prefix="/v1")
@@ -12,3 +13,4 @@ def init_route(app):
     app.register_blueprint(list_post_api, url_prefix="/v1/posts")
     app.register_blueprint(list_post_by_user_api, url_prefix="/v1")
     app.register_blueprint(like_post_api, url_prefix="/v1")
+    app.register_blueprint(get_post_api, url_prefix="/v1/posts")
